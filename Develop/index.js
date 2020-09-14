a9c25b857c5e1ee40b21c15ba743dc971bc7116c
guillenjoshua <guillenjoshua@gmail.com>
2020-09-13 21:41:59 -0500

    complete classes and methods

--- Develop/index.js
+++ Develop/index.js
@@ -25,3 +25,50 @@ class Employee {
     }
 
 }
+
+class Manager extends Employee {
+    constructor (name, id, email, officeNumber) {
+        super (name, id, email)
+        this.officeNumber = officeNumber
+    }
+    getOfficeNumber () {
+        return `${this.officeNumber}`
+    }
+
+    getRole() {
+        return 'Manager'
+    }
+}
+
+class Engineer extends Employee {
+    constructor (name, id, email, gitHub) {
+        super (name, id, email)
+        this.gitHub = gitHub
+    }
+    getGitHub () {
+        return `GitHub: ${this.getGitHub}`
+    }
+
+    getRole() {
+        return 'Engineer'
+    }
+}
+
+class Intern extends Employee {
+    constructor (name, id, email, school) {
+        super (name, id, email)
+        this.school = school
+    }
+    getSchool () {
+        return `School: ${this.school}`
+    }
+
+    getRole() {
+        return 'Intern'
+    }
+}
+
+const managerClass = new Manager ('Josh', 1, 'josh@email.com', 2020)
+const engineerClass = new Engineer ('Josh', 1, 'josh@email.com', 'guillenjoshua')
+const internClass = new Intern ('Josh', 1, 'josh@email.com', 'Texas Tech')
+console.log(internClass.getSchool())
